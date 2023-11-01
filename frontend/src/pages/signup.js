1b32062af0b679d9a461f981dbf268b783b2c333
TAEEUN KIM <taeeunk1208@gmail.com>
2023-10-31 20:10:45 -0400

    Update signup.js

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -20,7 +20,7 @@ const SignUp = () => {
             >
                 <div className="ride-emory">
                 {/* <img className="icon" src="../../pe-logo-w-border-small.png"> */}
-                    RIDEmory hello hi
+                    RIDEmory
                 </div>
                 <p className="small-text">Welcome! Let's get started!</p>
                 {/* <form id="sign-in"> */}
